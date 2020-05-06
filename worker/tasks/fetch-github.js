713ac7b06402d034db0bbb2cd3ad06590fa0f2dd
Gary Hawes <gary.hawes@gmail.com>
2020-05-06 21:45:15 +0100

    Filtering out senior positions

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -8,6 +8,7 @@ const setAsync = promisify(client.set).bind(client);
 
 const baseUrl = "https://jobs.github.com/positions.json";
 
+// fetch all pages.
 async function fetchGithub() {
   let resultCount = 1;
   let onPage = 0;
@@ -21,8 +22,27 @@ async function fetchGithub() {
     console.log("got:", jobs.length, "jobs");
     onPage++;
   }
-
   console.log("got:", allJobs.length, "Jobs Total");
+
+  // filter
+  const jrJobs = allJobs.filter((job) => {
+    const jobTitle = job.title.toLowerCase();
+
+    // algo logic
+    if (
+      jobTitle.includes("senior") ||
+      jobTitle.includes("manager") ||
+      jobTitle.includes("sr.") ||
+      jobTitle.includes("architect")
+    ) {
+      return false;
+    }
+    return true;
+  });
+
+  console.log("filtered down to", jrJobs.length);
+
+  // set in redis.
   const success = await setAsync("github", JSON.stringify(allJobs));
 
   console.log({ success });
