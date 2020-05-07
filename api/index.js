691a9d71e013d0e3472d1646147431d04453aa63
Gary Hawes <gary.hawes@gmail.com>
2020-05-07 21:49:53 +0100

    API now working on docker

--- api/index.js
+++ api/index.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const redis = require("redis");
+const client = redis.createClient(6379, "redis");
+
+const { promisify } = require("util");
+const getAsync = promisify(client.get).bind(client);
+
+app.get("/jobs", async (req, res) => {
+  const jobs = await getAsync("github");
+  return res.send(JSON.parse(jobs));
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
