45e5ccc64a6d5f7572c6673691cded5c0c0c6c7b
Gary Hawes <gary.hawes@gmail.com>
2020-05-09 21:46:18 +0100

    Bit of styling & adding starting point of pagination

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
 
 export default function Job({ job }) {
   return (
     <div className={"job"}>
-      <span>{job.title}</span>
-      <span>{job.company}</span>
-      <span>{job.location}</span>
+      <div>
+        <Typography variant="h6">{job.title}</Typography>
+        <Typography variant="h5">{job.company}</Typography>
+        <Typography>{job.location}</Typography>
+      </div>
+      <div>
+        <Typography>
+          {job.created_at.split(" ").splice(0, 3).join(" ")}
+        </Typography>
+      </div>
     </div>
   );
 }
--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -3,13 +3,48 @@ import { Typography } from "@material-ui/core";
 
 import Job from "./Job";
 
+import MobileStepper from "@material-ui/core/MobileStepper";
+import Button from "@material-ui/core/Button";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+
 export default function Jobs({ jobs }) {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
   return (
     <div className="jobs">
-      <Typography variant="h1">Entry Level Software Jobs</Typography>
+      <Typography variant="h4" component="h1">
+        Entry Level Software Jobs
+      </Typography>
       {jobs.map((job) => (
         <Job job={job} />
       ))}
+      <MobileStepper
+        variant="progress"
+        steps={6}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+            Next
+            <KeyboardArrowRight />
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            <KeyboardArrowLeft />
+            Back
+          </Button>
+        }
+      />
     </div>
   );
 }
